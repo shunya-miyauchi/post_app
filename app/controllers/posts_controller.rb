2f3d757380beca28b00f190b6a822a2f203a14ce
Miyauchi <hy.miyauchi@gmail.com>
2022-01-26 11:45:32 +0900

    一覧表示ページ作成(未デザイン)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,8 @@
 class PostsController < ApplicationController
-  
+  before_action :post_find, only: [:destroy]
+
   def new
     @post = Post.new
-  end
-
-  def index
-
   end
 
   def create
@@ -13,8 +10,22 @@ class PostsController < ApplicationController
     redirect_to new_post_path
   end
 
+  def index
+    @posts = Post.all
+  end
+
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def post_find
+    @post = Post.find(params[:id])
   end
 end
