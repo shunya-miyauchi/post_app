aa5b19f796da0e715e6d93c80034de92baba8478
Miyauchi <hy.miyauchi@gmail.com>
2022-01-26 10:26:51 +0900

    新規投稿ページ作成(未デザイン)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+  
+  def new
+    @post = Post.new
+  end
+
+  def index
+
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to new_post_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
